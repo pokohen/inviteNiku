d93b46ce08d41e7f307f5ad2fc8433ac28fb9575
gimjinhyeon <canzoo@imweb.me>
2025-01-23 18:25:47 +0900

    add. info add

--- src/conpoments/InvitedDetail.tsx
+++ src/conpoments/InvitedDetail.tsx
@@ -20,29 +20,42 @@ function InvitedDetail (props: invitedInfo) {
         <>
             <FireworkCanvas />
             <Typography
-                variant="body1"
-                style={{
-                    textAlign: "center",
-                    position: 'relative',
-                    zIndex: 0,
-                }}
+              variant="body1"
+              style={{
+                textAlign: 'center',
+                position: 'relative',
+                zIndex: 0,
+              }}
             >
-                <Typography 
-                    variant="h5"
-                    style={{
-                        fontWeight: "bold",
-                        color: colors[Math.round(colors.length * Math.random())]
-                    }}
+                <Typography
+                  variant="h5"
+                  style={{
+                    fontWeight: 'bold',
+                    color: colors[Math.round(colors.length * Math.random())],
+                  }}
                 >
                     {props.invitedName}様、
                 </Typography>
-                招待に応じてくださって<br/>
-                ありがとうございます。🙇‍♂️<br/><br/>
 
-                日程：令和7年一月二十五日<br/>
-                時間：17時から<br/>
-                場所：パーティールーム・オンダム<br/>
-                マップ：<a href='https://naver.me/GMm8ttNp' target='_blank'>Naver Map</a>
+                  招待に応じてくださって<br />
+                  ありがとうございます。🙇‍♂️<br /><br />
+
+                  日程：令和7年一月二十五日<br />
+                  時間：17時から<br />
+                  場所：パーティールーム・オンダム<br />
+                  マップ：<a href="https://naver.me/GMm8ttNp" target="_blank">Naver Map</a><br /><br />
+
+                  飲み物とか食べ物は自分で自由に持ってきてください。<br />
+                  身分証明書などは必要ありません。<br /><br />
+                  このパーティーについてお問い合わせがある場合は、<br />
+                  以下の連絡先にお問い合わせください。<br /><br />
+
+                  そしてスペシャルゲストが来るかもしれません。<br />
+                  名前は幸運です！<br />
+                  お楽しみに！🎉<br /><br />
+
+                  連絡先：010-2649-7023<br />
+                  担当：キム・ジンヒョン<br /><br />
 
             </Typography>
             <Button
